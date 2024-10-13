d5143d00ebd4515ee9ba4bf0c20f67009498fc57
leotorres9286 <ltg9286@gmail.com>
2024-10-13 02:43:39 -0400

    Feat: Add switch theme.

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -3,11 +3,13 @@
 import Link from 'next/link';
 import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
-import { AppTopbarRef } from '@/types';
+import { AppTopbarRef, LayoutConfig } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
+import { PrimeReactContext } from 'primereact/api';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
-    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
+    const { layoutConfig, setLayoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
+    const { changeTheme } = useContext(PrimeReactContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
@@ -17,6 +19,20 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         topbarmenu: topbarmenuRef.current,
         topbarmenubutton: topbarmenubuttonRef.current
     }));
+
+    const _changeTheme = () => {
+        let themeConfig = { colorScheme: layoutConfig.colorScheme, theme: layoutConfig.theme };
+        if (layoutConfig.colorScheme === 'light') {
+            themeConfig.colorScheme = 'dark';
+            themeConfig.theme = 'lara-dark-blue';
+        } else {
+            themeConfig.colorScheme = 'light';
+            themeConfig.theme = 'lara-light-blue';
+        }
+        changeTheme?.(layoutConfig.theme, themeConfig.theme, 'theme-css', () => {
+            setLayoutConfig((prevState: LayoutConfig) => ({ ...prevState, theme: themeConfig.theme, colorScheme: themeConfig.colorScheme }));
+        });
+    };
 
     return (
         <div className="layout-topbar">
@@ -34,9 +50,9 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
             </button>
 
             <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
-                <button type="button" className="p-link layout-topbar-button">
-                    <i className="pi pi-calendar"></i>
-                    <span>Calendar</span>
+                <button type="button" className="layout-topbar-button layout-topbar-button-theme" onClick={_changeTheme}>
+                    <i className="pi pi-palette"></i>
+                    <span>Cambiar tema</span>
                 </button>
                 <button type="button" className="p-link layout-topbar-button">
                     <i className="pi pi-user"></i>

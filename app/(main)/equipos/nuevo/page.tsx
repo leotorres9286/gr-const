1c0a0ad77a6237afb67dfa8716524bca749ca96e
leotorres9286 <ltg9286@gmail.com>
2024-10-12 23:48:46 -0400

    Feat: Add validation to New Equipment form. (clean code)

--- app/(main)/equipos/nuevo/page.tsx
+++ app/(main)/equipos/nuevo/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
